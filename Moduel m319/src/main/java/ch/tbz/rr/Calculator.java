4b8f87750aec4331659f8f61b85d21b149129ba0
Robin-Rupp <90399801+Robin-Rupp@users.noreply.github.com>
2021-12-02 11:18:41 +0100

    1.Update

---
+++ Moduel m319/src/main/java/ch/tbz/rr/Calculator.java
@@ -0,0 +1,47 @@
+
+package ch.tbz.rr;
+
+
+import static java.lang.System.*;
+
+import static ch.tbz.rr.Input.*;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        out.println("Kontrollstrukturen kombineren mit benötigten Daten:");
+        while (true) {
+            double dbiX = inputInt("Geben Sie die 1. Zahl ein: ");
+            double dbiY = inputInt("Geben Sie die 2. Zahl ein: ");
+            String operator = inputString("What operator?");
+            if ((dbiX == 0 || dbiY == 0) && (operator.equals("/") || operator.equals("&"))){
+                out.println("Division durch Null geht nicht");
+                continue;
+            }
+            double result = switch (operator) {
+                case "+" -> dbiX + dbiY;
+                case "-" -> dbiX - dbiY;
+                case "*" -> dbiX * dbiY;
+                case "/" -> dbiX / dbiY;
+                case "%" -> dbiX % dbiY;
+                default -> 0.0;
+            };
+
+            out.println("Das Resultat ist: " + result);
+            double dbiZ = inputInt("Geben Sie die 3. Zahl ein: ");
+            switch (operator) {
+                case "+" -> result += dbiZ;
+                case "-" -> result -= dbiZ;
+                case "*" -> result *= dbiZ;
+                case "/" -> result /= dbiZ;
+                case "%" -> result %= dbiZ;
+            }
+            out.println("Das Resultat ist: " + result);
+            String answer = inputString("What to play again? N/y");
+            if (answer.equals("N")){
+                break;
+            }
+        }
+    }
+}
